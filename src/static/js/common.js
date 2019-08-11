1e276b017a53590ffb6b5ced96cfadc316b8f31d
Aslan <berezg86@gmail.com>
2019-08-11 15:33:48 +0300

    шапка, футер, слайдер на главной

---
+++ src/static/js/common.js
@@ -0,0 +1,50 @@
+$(document).ready(function() {
+    $('.tooltip-btn').hover(function() {
+        $('.tooltip-header').toggleClass('block-show');
+    });
+
+    $(function() {
+        $('.burger').click(function() {
+            $('.main-nav ul').slideToggle();
+        });
+        $(window).resize(function() {
+            var w = $(window).width();
+            if (w >= 768) {
+                $('.main-nav ul').removeAttr('style');
+            }
+        });
+    });
+
+    $('.main-slider').owlCarousel({
+        center: true,
+        items:1,
+        loop:true,
+        margin:5,
+        nav: true,
+        autoplay: true,
+        navText: ['<span class="nav-left"></span>', '<span class="nav-right"></span>'],
+        smartSpeed: 1000,
+        responsive:{
+            768:{
+                items:2
+            },
+            992:{
+                items:4
+            }
+        }
+    });
+
+    $('.main-slider_item').hover(function(){
+        $(this).find('.main-slider_hover').stop().slideToggle();
+    });
+
+
+
+
+
+
+
+
+
+
+});
